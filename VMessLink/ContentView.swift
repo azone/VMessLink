27a5a124e1dcb014f83c360ede831919cce7cdbd
Yozone Wang <wangyaozh@gmail.com>
2021-02-03 20:52:01 +0800

    Initial Commit

---
+++ VMessLink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VMessLink
+//
+//  Created by 王要正 on 2021/2/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VMessLink/VMessLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VMessLinkApp.swift
+//  VMessLink
+//
+//  Created by 王要正 on 2021/2/3.
+//
+
+import SwiftUI
+
+@main
+struct VMessLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
